e02142d73bfa83577530ff1bd13e7fbff6ea3510
MarcMouries <marc@mouries.net>
2024-07-28 15:19:40 -0400

    styling

--- src/components/app-content.js
+++ src/components/app-content.js
@@ -1,7 +1,16 @@
+// src/components/app-content.js
+import { EVENT_RECORD_LIST_UPDATED } from '../EventTypes.ts';
+import { EventEmitter } from '../EventEmitter.ts';
+
 class AppContent extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+
+    EventEmitter.on(EVENT_RECORD_LIST_UPDATED, ({ itemName, records }) => {
+      console.log(`%c⑩ Updating content for: ${itemName}`, 'color: white; background: darkblue;');
+      this.setContent(itemName, records);
+    });
 
     const content = document.createElement('div');
     content.setAttribute('class', 'content');
@@ -9,19 +18,30 @@ class AppContent extends HTMLElement {
 
     const style = document.createElement('style');
     style.textContent = `
+
+    :host {
+
+      background-color: #E5E9FE
+    }
       .content {
         padding: 20px;
         font-size: 16px;
         line-height: 1.5;
+        background-color: white;
+        border-radius: 10px;
+        box-shadow: 0 2px 4px rgba(0,0,0,0.1);
       }
       .header-row {
         display: flex;
         justify-content: space-between;
         align-items: center;
+        margin-bottom: 20px;
       }
       .header-row h2 {
         margin: 0;
-      }
+        font-size: 1.5em;
+        color: #4A47A3;
+            }
       table {
         width: 100%;
         border-collapse: collapse;
@@ -30,12 +50,14 @@ class AppContent extends HTMLElement {
       th, td {
         padding: 10px;
         border: 1px solid #ddd;
+        text-align: left;
       }
       th {
         background-color: #f4f4f4;
+        color: #4A47A3;
       }
       .content button {
-        background-color: #007bff;
+        background-color: #4A47A3;
         color: white;
         border: none;
         padding: 10px 20px;
@@ -45,6 +67,9 @@ class AppContent extends HTMLElement {
     `;
     this.shadowRoot.appendChild(style);
   }
+
+
+
 
   setContent(appName, data) {
     const contentDiv = this.shadowRoot.querySelector('.content');
