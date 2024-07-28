57eb0eb16f02bb780172a2d853eb471773d67401
MarcMouries <marc@mouries.net>
2024-07-28 12:57:52 -0400

    new

---
+++ src/EventTypes.ts
@@ -0,0 +1,5 @@
+// src/EventTypes.ts
+export const EVENT_USER_AUTHENTICATED = 'userAuthenticated';
+export const EVENT_AUTH_FAILED = 'authFailed';
+export const EVENT_USER_APPS_FETCHED = 'userAppsFetched';
+export const EVENT_APP_SELECTED = 'appSelected';
