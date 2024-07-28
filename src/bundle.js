dcfe32bb1bc5560701e284bdcc2f1a2d39e87f6c
MarcMouries <marc@mouries.net>
2024-07-28 15:14:08 -0400

    new

---
+++ src/bundle.js
@@ -0,0 +1,15 @@
+import './components/app-nav.js';
+import './components/app-content.js';
+import './components/online-status.js';
+import './app-model.js';
+import './app-view.js';
+import './app-controller.js';
+import './dataService';
+import './login.js';
+import './main.js';
+import './EventEmitter';
+import './EventTypes';
+import './main.js';
+import { LOG_STYLE } from './LogStyles';
+
+console.log('%c⑯ Bundle loaded', LOG_STYLE);
