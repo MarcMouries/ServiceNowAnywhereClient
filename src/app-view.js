2b80005ed7ac532ee9f784b6c3694d9379e98c9b
MarcMouries <marc@mouries.net>
2024-07-31 08:32:04 -0400

    use template

--- src/app-view.js
+++ src/app-view.js
@@ -1,44 +1,41 @@
 // src/components/app-view.js
-import { initializeApp } from './app-controller';
-import { LOG_STYLE } from './LogStyles.ts';
-import { EventEmitter } from './EventEmitter.ts';
+import { initializeApp } from "./app-controller";
+import { LOG_STYLE } from "./LogStyles.ts";
+import { EventEmitter } from "./EventEmitter.ts";
+
+const template = document.createElement("template");
+template.innerHTML = `
+<app-nav home-location="./workspace.html"></app-nav>
+<app-content></app-content>
+    <style>
+        :host {
+  display: flex;
+  height: 100vh;
+  width: 100%;
+}
+  app-nav {
+    width: 20%;
+    border-right: 1px solid #ddd;
+
+  }
+  app-content {
+    width: 80%;
+    padding: 20px;
+  }
+`;
 
 class AppView extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: 'open' });
-
-    const nav = document.createElement('app-nav');
-    const content = document.createElement('app-content');
-    this.shadowRoot.appendChild(nav);
-    this.shadowRoot.appendChild(content);
-
-    const style = document.createElement('style');
-    style.textContent = `
-    :host {
-      display: flex;
-      height: 100vh;
-      width: 100%;
-      background-color: #f3f4f6;
-    }
-      app-nav {
-        width: 20%;
-        border-right: 1px solid #ddd;
-
-      }
-      app-content {
-        width: 80%;
-        padding: 20px;
-      }
-    `;
-    this.shadowRoot.appendChild(style);
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
-    console.log('%c⑨ App View connected', LOG_STYLE);
-    console.log('%c⑩ Calling initializeApp', LOG_STYLE);
+    console.log("%c⑨ App View connected", LOG_STYLE);
+    console.log("%c⑩ Calling initializeApp", LOG_STYLE);
     initializeApp();
   }
 }
 
-customElements.define('app-view', AppView);
+customElements.define("app-view", AppView);
