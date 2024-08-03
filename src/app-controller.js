c904a599be57391dae076b25d8f8b7c898525dfb
MarcMouries <marc@mouries.net>
2024-08-02 21:58:33 -0400

    use dataService

--- src/app-controller.js
+++ src/app-controller.js
@@ -1,6 +1,9 @@
 // src/app-controller.js
 import { Model } from "./app-model";
-import { authenticateUser, fetchUserApps, fetchData } from "./dataService";
+//import { authenticateUser, fetchUserApps, fetchData } from "./DataSource";
+import { dataService } from './DataService';
+import { MockDataSource } from './datasource/mockDataSource';
+
 import { LOG_STYLE } from "./LogStyles";
 import { EVENT_SYS_AUTHENTICATED_USER, EVENT_AUTH_FAILED, EVENT_SYS_FETCHED_USER_APPS, 
   EVENT_USER_CLICKED_ON_APP, EVENT_USER_CLICKED_RECORD_ROW, EVENT_USER_CLICKED_NEW_RECORD_BUTTON } from './EventTypes';
@@ -8,8 +11,16 @@ import { EventEmitter } from "./EventEmitter";
 
 const model = new Model();
 
+const isMock = true;
+
+if (isMock) {
+  dataService.setDataSource(new MockDataSource());
+  console.log('dataService......:', dataService);
+
+}
+
 export async function initializeApp() {
-  console.log("%c① Initializing App", LOG_STYLE);
+  console.log("%c① ① Initializing App", LOG_STYLE);
   const user = JSON.parse(localStorage.getItem("user"));
   if (user && user.sys_id) {
     model.setUser(user);
@@ -20,15 +31,13 @@ export async function initializeApp() {
 
 EventEmitter.on(EVENT_SYS_AUTHENTICATED_USER, async (user) => {
   console.log(`%c③ User successfully authenticated: ${JSON.stringify(user)}`, LOG_STYLE);
-  const itemList = await fetchUserApps(user.sys_id);
+  const itemList = await dataService.fetchUserApps(user.sys_id);
   console.log(`%c⑥ Apps fetched for user: ${user.name} = ${itemList.join(", ")}`, LOG_STYLE);
   model.setItemList(itemList);
 
   // Redirect to workspace.html
-  console.log(`%c⑦ Redirect to workspace ??`, LOG_STYLE);
-  console.log(`%c⑦ window.location = ${window.location}`, LOG_STYLE);
-
-
+  console.log(`%c⑦ Current location ${window.location.href}`, LOG_STYLE);
+  
   if (window.location.pathname == "/") {
     console.log(`%c⑦ Redirect to workspace.html`, LOG_STYLE);
     window.location.href = "./workspace.html";
@@ -41,7 +50,7 @@ EventEmitter.on(EVENT_AUTH_FAILED, (message) => {
 
 EventEmitter.on(EVENT_USER_CLICKED_ON_APP, async (dataName) => {
   console.log(`%c⑧ Handling data selection: ${dataName}`, LOG_STYLE);
-  const selectedData = await fetchData(dataName);
+  const selectedData = await dataService.fetchData(dataName);
   model.setItemRecordList(dataName, selectedData);
 });
 
