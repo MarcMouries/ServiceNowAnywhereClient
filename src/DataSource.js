038d0162a9ad29feb52d4e4229761ef864943995
MarcMouries <marc@mouries.net>
2024-08-02 21:54:29 -0400

    new

---
+++ src/DataSource.js
@@ -0,0 +1,14 @@
+// src/dataSource.js
+export class DataSource {
+    async authenticateUser(username, password) {
+      throw new Error('authenticateUser method not implemented');
+    }
+
+    async fetchUserApps(userSysId) {
+      throw new Error('fetchUserApps method not implemented');
+    }
+
+    async fetchData(appName) {
+      throw new Error('fetchData method not implemented');
+    }
+  }
