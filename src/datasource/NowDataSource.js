e75975472277601c849cf8f206c1a330d71416e3
MarcMouries <marc@mouries.net>
2024-08-06 10:13:26 -0400

    authenticateUser

--- src/datasource/NowDataSource.js
+++ src/datasource/NowDataSource.js
@@ -5,17 +5,58 @@ import { EVENT_SYS_AUTHENTICATED_USER, EVENT_AUTH_FAILED, EVENT_SYS_FETCHED_USER
 
 export class NowDataSource extends DataSource {
   async authenticateUser(username, password) {
-    console.log("Using mock data source for authentication");
-    const user = {
-      name: "Mock User",
-      email: "mock@example.com",
-      sys_id: "mock-sys-id",
-      authToken: "mock-token",
-    };
+    console.log("Using NowDataSource data source for authentication");
 
-    window.NOW_ANYWHERE.user = user;
-    localStorage.setItem("user", JSON.stringify(user));
-    EventEmitter.emit(EVENT_SYS_AUTHENTICATED_USER, user);
+    try {
+      const { servicenowUrl } = window.NOW_ANYWHERE;
+      const credentials = btoa(`${username}:${password}`);
+      const table = "sys_user";
+      const sysparm_fields = "name,email,sys_id";
+      const sysparm_limit = 1;
+
+      const url = `${servicenowUrl}/api/now/table/${table}?sysparm_query=user_name=${username}&sysparm_fields=${encodeURIComponent(sysparm_fields)}&sysparm_limit=${sysparm_limit}`;
+      console.log("url", url);
+
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: `Basic ${credentials}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log("Response status:", response.status);
+      console.log("Response headers:", response.headers);
+
+      if (response.ok) {
+        const data = await response.json();
+        const user = {
+          name: data.result[0].name,
+          email: data.result[0].email,
+          sys_id: data.result[0].sys_id,
+          authToken: credentials,
+        };
+        console.log("User data:", user);
+
+        // Save to window.NOW_ANYWHERE
+        window.NOW_ANYWHERE.user = user;
+        localStorage.setItem("user", JSON.stringify(user));
+        EventEmitter.emit(EVENT_SYS_AUTHENTICATED_USER, user);
+      } else {
+        const errorText = await response.text();
+        console.error("Login failed:", errorText);
+        document.getElementById("error-msg").textContent = `Login failed: ${errorText}`;
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      console.error(`Error.message= ${error.message}`);
+      if (error instanceof TypeError && error.message.includes("Load failed")) {
+        document.getElementById("error-msg").textContent = "Unable to connect to the server. Please check your network connection.";
+      } else {
+        document.getElementById("error-msg").textContent = `Error: ${error.message}`;
+      }
+    }
   }
 
   async fetchUserApps(userSysId) {
