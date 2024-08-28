612cf26bbad924ff921ebaff1c5ee31caa82dc3a
MarcMouries <marc@mouries.net>
2024-08-27 20:25:57 -0400

    handle table selection

--- src/app-controller.js
+++ src/app-controller.js
@@ -8,7 +8,10 @@ import {
   EVENT_AUTH_FAILED,
   EVENT_SYS_AUTHENTICATED_USER,
   EVENT_SYS_FETCHED_USER_APPS,
+  EVENT_SYS_FETCHED_USER_TABLES,
   EVENT_USER_CLICKED_ON_APP,
+  EVENT_USER_CLICKED_ON_TABLE,
+  EVENT_RECORD_LIST_UPDATED,
   EVENT_USER_CLICKED_RECORD_ROW,
   EVENT_USER_CLICKED_NEW_RECORD_BUTTON,
 } from "./EventTypes";
@@ -37,17 +40,20 @@ export async function initializeApp() {
 }
 
 EventEmitter.on(EVENT_SYS_AUTHENTICATED_USER, async (user) => {
-  console.log(`%c③ User successfully authenticated: ${JSON.stringify(user)}`, LOG_STYLE);
-  const appList = await dataService.fetchUserApps(user.sys_id);
-  console.log(`%c⑥ Apps fetched for user: ${user.name} = ${appList.join(", ")}`, LOG_STYLE);
-  model.setUserAppsList(appList);
+  console.log(`%c③ Controller User successfully authenticated: ${JSON.stringify(user)}`, LOG_STYLE);
+  const userAppsList = await dataService.fetchUserApps(user.sys_id);
+  const appNames = userAppsList.map((app) => app.appName);
+  console.log(`%c⑥ Controller: Apps fetched for user: ${user.name} = ${appNames.join(", ")}`, LOG_STYLE);
+  model.setUserAppsList(userAppsList);
+
+  console.log(`%c⑦ Controller: Current location ${window.location.href}`, LOG_STYLE);
+  EventEmitter.emit(EVENT_SYS_FETCHED_USER_APPS, { appList: userAppsList });
 
   // Redirect to workspace.html
-  console.log(`%c⑦ Current location ${window.location.href}`, LOG_STYLE);
-
   if (window.location.pathname == "/") {
-    console.log(`%c⑦ Redirect to workspace.html`, LOG_STYLE);
+    console.log(`%c⑦ Controller: Redirect to workspace.html`, LOG_STYLE);
     window.location.href = "./workspace.html";
+
   }
 });
 
@@ -55,10 +61,18 @@ EventEmitter.on(EVENT_AUTH_FAILED, (message) => {
   document.getElementById("error-msg").textContent = message;
 });
 
-EventEmitter.on(EVENT_USER_CLICKED_ON_APP, async (app) => {
-  console.log(`%c⑧ Handling app selection: ${app.appName}`, LOG_STYLE);
-  const tableList = await dataService.fetchTablesForApp(app.appScope);
-  model.setUserTablesList(app.appScope, tableList);
+EventEmitter.on(EVENT_USER_CLICKED_ON_APP, async (payload) => {
+  console.log(`%c⑧ App controller: Handling app selection: ${payload.appName}`, LOG_STYLE);
+  const tableList = await dataService.fetchTablesForApp(payload.appScope);
+  model.setUserTablesList(payload.appScope, tableList);
+  EventEmitter.emit(EVENT_SYS_FETCHED_USER_TABLES, { appScope: payload.appScope, tableList });
+});
+
+EventEmitter.on(EVENT_USER_CLICKED_ON_TABLE, async (payload) => {
+  console.log(`%c⑧ App controller: Handling Table Selection: ${payload.tableName}`, LOG_STYLE);
+  const recordsList = await dataService.fetchRecordsForTable(payload.tableName);
+  model.setUserTableRecordList(payload.tableName, recordsList);
+  EventEmitter.emit(EVENT_RECORD_LIST_UPDATED, { tableName: payload.tableName, recordsList });
 });
 
 EventEmitter.on(EVENT_USER_CLICKED_NEW_RECORD_BUTTON, (appName) => {
