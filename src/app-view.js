c183f0106136556aff0765db8813a6d99a186448
MarcMouries <marc@mouries.net>
2024-09-09 21:49:00 -0400

    handle routes

--- src/app-view.js
+++ src/app-view.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "./app-controller";
 import { LOG_STYLE } from "./LogStyles.ts";
 import { EventEmitter } from "./EventEmitter.ts";
+import Router from "./components/router";
 
 const template = document.createElement("template");
 template.innerHTML = `
@@ -15,10 +16,9 @@ template.innerHTML = `
       width: 20%;
   }
   main {
-      width: 80%;
-      padding: 10px;
       background: slategray;
       display: flex;
+      width: 80%;
 }
 .hidden {
   display: none;
@@ -27,11 +27,10 @@ template.innerHTML = `
 </style>
 
 <side-bar home-location="./workspace.html"></side-bar>
-<main>
-  <list-view></list-view>
-  <record-view></record-view>
-</main>
-`;
+<main></main>`;
+
+// <list-view></list-view>
+// <record-view></record-view>
 
 class AppView extends HTMLElement {
   constructor() {
@@ -44,6 +43,46 @@ class AppView extends HTMLElement {
     console.log("%c⑨ App View connected", LOG_STYLE);
     console.log("%c⑩ Calling initializeApp", LOG_STYLE);
     initializeApp();
+
+
+    const routes = {
+      '/list/:table': ({ table, tableData }) => {
+          this.showTableContent(table, tableData);
+      },
+      '/record/:table/:sysId': ({ table, sysId, recordData }) => {
+          this.showRecordView(table, sysId, recordData);
+      }
+    };
+  
+    const router = new Router(routes);
+
+    this.attachEventListeners(router);
+  }
+
+  showTableContent(table, tableData) {
+    const main = this.shadowRoot.querySelector("main");
+    main.innerHTML = `<list-view></list-view>`;
+    const listView = main.querySelector("list-view");
+    if (listView) {
+      listView.setContent(table, tableData);
+    }
+  }
+
+  showRecordView(table, sysId, recordData) {
+    const main = this.shadowRoot.querySelector("main");
+    main.innerHTML = `<record-view></record-view>`;
+    const recordView = main.querySelector("record-view");
+    if (recordView) {
+      recordView.displayRecord(table, recordData);
+    }
+  }
+
+  attachEventListeners(router) {
+    EventEmitter.on("navigate", (path, payload) => {
+      console.log("App View received Navigate event for path: " + path);
+
+      router.navigate(path, payload);
+    });
   }
 }
 
